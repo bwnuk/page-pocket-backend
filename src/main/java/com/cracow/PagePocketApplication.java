c8c8161bf9e1cd063e64b748e3b48bbb19267caf
bwnuk <Bartekwnuk13>
2019-03-29 22:34:38 +0100

    Init project.

---
+++ src/main/java/com/cracow/PagePocketApplication.java
@@ -0,0 +1,13 @@
+package com.cracow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagePocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagePocketApplication.class, args);
+	}
+
+}
