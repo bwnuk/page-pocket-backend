43ba0a519da9c3ee9aea5187b62ce0483c5def50
daterka <fallowdeer.mail@gmail.com>
2019-04-20 15:27:53 +0200

    Add UNAUTHORIZED_LOGIN_LOGOUT_FAILED errror code

--- src/main/java/com/cracow/web/exception/ServiceErrorCode.java
+++ src/main/java/com/cracow/web/exception/ServiceErrorCode.java
@@ -5,7 +5,8 @@ public enum ServiceErrorCode {
     NOT_FOUND(1),
     ALREADY_EXIST(2),
     NOT_MODIFY(3),
-    CONNECTION_FAILED(4);
+    CONNECTION_FAILED(4),
+    UNAUTHORIZED_LOGIN_LOGOUT_FAILED(5);
 
     private final int code;
 
