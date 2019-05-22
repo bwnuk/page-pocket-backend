c7db4ce48b098e7cdd3cd7f83c0b02748cb42212
Nimander <szymanskimichal57@gmail.com>
2019-05-22 20:24:28 +0200

    cleanup

--- src/main/java/com/cracow/service/BookmarkService.java
+++ src/main/java/com/cracow/service/BookmarkService.java
@@ -23,117 +23,118 @@ import java.util.stream.Collectors;
 @Service
 public class BookmarkService {
 
-    private final BookmarkRepository bookmarkRepository;
-    private final SecurityService securityService;
-    private final UserService userService;
-    private final ParserService parserService;
+	private final BookmarkRepository bookmarkRepository;
+	private final SecurityService securityService;
+	private final UserService userService;
+	private final ParserService parserService;
 
-    public BookmarkService(BookmarkRepository bookmarkRepository, SecurityService securityService, UserService userService, ParserService parserService) {
-        this.bookmarkRepository = bookmarkRepository;
-        this.securityService = securityService;
-        this.userService = userService;
-        this.parserService = parserService;
-    }
+	public BookmarkService(BookmarkRepository bookmarkRepository, SecurityService securityService, UserService userService, ParserService parserService) {
+		this.bookmarkRepository = bookmarkRepository;
+		this.securityService = securityService;
+		this.userService = userService;
+		this.parserService = parserService;
+	}
 
-    public Set<BookmarkDto> findAll(Optional<String> tag) {
-        String email = securityService.findLoggedInEmail();
-        UserEntity user = userService.findByEmailOrThrow404(email);
-        Map<String, Set<String>> bookmarks = user.getBookmarksListMap();
-        Collection<Set<String>> lists = bookmarks.values();
-        List<String> bookmarkIDs = Lists.newArrayList(Iterables.concat(lists));
+	public Set<BookmarkDto> findAll(Optional<String> tag) {
+		String email = securityService.findLoggedInEmail();
+		UserEntity user = userService.findByEmailOrThrow404(email);
+		Map<String, Set<String>> bookmarks = user.getBookmarksListMap();
+		Collection<Set<String>> lists = bookmarks.values();
 
-
-        List<BookmarkEntity> bookmarkEntities;
-        if (tag.isPresent()) {
-            String tagName = tag.get();
-            bookmarkEntities = bookmarkRepository.findByTagsInAndIdIn(Lists.newArrayList(tagName), bookmarkIDs);
-        } else {
-            bookmarkEntities = Lists.newArrayList(bookmarkRepository.findAllById(bookmarkIDs));
-        }
-
-        return bookmarkEntities.stream().map(bookmarkEntity -> bookmarkEntity.toDto()).collect(Collectors.toSet());
-    }
-
-    public BookmarkDto saveBookmark(BookmarkNewDto bookmarkNewDto, boolean parse) {
-        String email = securityService.findLoggedInEmail();
-        UserEntity user = userService.findByEmailOrThrow404(email);
-
-        String title = bookmarkNewDto.getTitle();
-        Set<String> tags = bookmarkNewDto.getTags();
-        Map<String, Set<String>> userBookmarkMap = user.getBookmarksListMap();
-
-        if (bookmarkRepository.findByTitle(title).isPresent()) {
-            throw new ConflictProblem("bookmark", "title", title);
-        }
-
-        BookmarkEntity bookmarkEntity;
-        if (parse) {
-            byte[] blob = parserService.parseToByte(bookmarkNewDto.getSource());
-            bookmarkEntity = new BookmarkEntity(bookmarkNewDto, blob);
-        } else {
-            bookmarkEntity = new BookmarkEntity(bookmarkNewDto);
-        }
-        bookmarkRepository.save(bookmarkEntity);
+		List<String> bookmarkIDs = Lists.newArrayList(Iterables.concat(lists));
 
 
-        tags.forEach(tag -> {
-            Set<String> list = userBookmarkMap.getOrDefault(tag, new HashSet<>());
-            list.add(bookmarkEntity.getId());
-            userBookmarkMap.put(tag, list);
-        });
-        userService.saveUser(user);
+		List<BookmarkEntity> bookmarkEntities;
+		if (tag.isPresent()) {
+			String tagName = tag.get();
+			bookmarkEntities = bookmarkRepository.findByTagsInAndIdIn(Lists.newArrayList(tagName), bookmarkIDs);
+		} else {
+			bookmarkEntities = Lists.newArrayList(bookmarkRepository.findAllById(bookmarkIDs));
+		}
 
-        return bookmarkEntity.toDto();
-    }
+		return bookmarkEntities.stream().map(bookmarkEntity -> bookmarkEntity.toDto()).collect(Collectors.toSet());
+	}
 
-    public BookmarkBlobDto findById(String id) {
-        String email = securityService.findLoggedInEmail();
-        UserEntity user = userService.findByEmailOrThrow404(email);
+	public BookmarkDto saveBookmark(BookmarkNewDto bookmarkNewDto, boolean parse) {
+		String email = securityService.findLoggedInEmail();
+		UserEntity user = userService.findByEmailOrThrow404(email);
 
-        throw401IfAccessDenied(user, id);
+		String title = bookmarkNewDto.getTitle();
+		Set<String> tags = bookmarkNewDto.getTags();
+		Map<String, Set<String>> userBookmarkMap = user.getBookmarksListMap();
 
-        BookmarkEntity bookmarkEntity = findByIdOrThrow401(id);
+		if (bookmarkRepository.findByTitle(title).isPresent()) {
+			throw new ConflictProblem("bookmark", "title", title);
+		}
 
-        return bookmarkEntity.toBlobDto();
-    }
+		BookmarkEntity bookmarkEntity;
+		if (parse) {
+			byte[] blob = parserService.parseToByte(bookmarkNewDto.getSource());
+			bookmarkEntity = new BookmarkEntity(bookmarkNewDto, blob);
+		} else {
+			bookmarkEntity = new BookmarkEntity(bookmarkNewDto);
+		}
+		bookmarkRepository.save(bookmarkEntity);
 
-    public BookmarkBlobDto parseLink(String id) {
-        String email = securityService.findLoggedInEmail();
-        UserEntity user = userService.findByEmailOrThrow404(email);
 
-        throw401IfAccessDenied(user, id);
+		tags.forEach(tag -> {
+			Set<String> list = userBookmarkMap.getOrDefault(tag, new HashSet<>());
+			list.add(bookmarkEntity.getId());
+			userBookmarkMap.put(tag, list);
+		});
+		userService.saveUser(user);
 
-        BookmarkEntity bookmarkEntity = findByIdOrThrow401(id);
-        byte[] bytes = parserService.parseToByte(bookmarkEntity.getSource());
-        bookmarkEntity.setBlob(bytes);
+		return bookmarkEntity.toDto();
+	}
 
-        bookmarkRepository.save(bookmarkEntity);
-        return bookmarkEntity.toBlobDto();
-    }
+	public BookmarkBlobDto findById(String id) {
+		String email = securityService.findLoggedInEmail();
+		UserEntity user = userService.findByEmailOrThrow404(email);
 
-    public void deleteById(String id) {
-        String email = securityService.findLoggedInEmail();
-        UserEntity user = userService.findByEmailOrThrow404(email);
+		throw401IfAccessDenied(user, id);
 
-        throw401IfAccessDenied(user, id);
+		BookmarkEntity bookmarkEntity = findByIdOrThrow401(id);
 
-        bookmarkRepository.deleteById(id);
-    }
+		return bookmarkEntity.toBlobDto();
+	}
 
-    private void throw401IfAccessDenied(UserEntity user, String bookmarkId) {
-        Map<String, Set<String>> userBookmark = user.getBookmarksListMap();
+	public BookmarkBlobDto parseLink(String id) {
+		String email = securityService.findLoggedInEmail();
+		UserEntity user = userService.findByEmailOrThrow404(email);
 
-        boolean isAllowed = userBookmark.values().stream().anyMatch(e -> e.contains(bookmarkId));
-        if (!isAllowed) {
-            throw new UnauthorizedProblem("Access is denied");
-        }
-    }
+		throw401IfAccessDenied(user, id);
 
-    private BookmarkEntity findByIdOrThrow401(String id) {
-        return bookmarkRepository
-                .findById(id)
-                .orElseThrow(() -> new NotFoundProblem("bookmark", "id", id));
-    }
+		BookmarkEntity bookmarkEntity = findByIdOrThrow401(id);
+		byte[] bytes = parserService.parseToByte(bookmarkEntity.getSource());
+		bookmarkEntity.setBlob(bytes);
+
+		bookmarkRepository.save(bookmarkEntity);
+		return bookmarkEntity.toBlobDto();
+	}
+
+	public void deleteById(String id) {
+		String email = securityService.findLoggedInEmail();
+		UserEntity user = userService.findByEmailOrThrow404(email);
+
+		throw401IfAccessDenied(user, id);
+
+		bookmarkRepository.deleteById(id);
+	}
+
+	private void throw401IfAccessDenied(UserEntity user, String bookmarkId) {
+		Map<String, Set<String>> userBookmark = user.getBookmarksListMap();
+
+		boolean isAllowed = userBookmark.values().stream().anyMatch(e -> e.contains(bookmarkId));
+		if (!isAllowed) {
+			throw new UnauthorizedProblem("Access is denied");
+		}
+	}
+
+	private BookmarkEntity findByIdOrThrow401(String id) {
+		return bookmarkRepository
+				.findById(id)
+				.orElseThrow(() -> new NotFoundProblem("bookmark", "id", id));
+	}
 
 
 }
